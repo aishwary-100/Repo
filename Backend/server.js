e1d96fed6f714a7872a535bf806e2a79158a48c9
Aishwary Tiwari <157511718+aishwary-100@users.noreply.github.com>
2025-07-13 21:30:44 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/books', require('./routes/books'));
-app.use('/api/mybooks', require('./routes/mybooks'));
+app.use('/api/myBooks', require('./routes/myBooks'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
